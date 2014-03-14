5873f7415c17e8445b4dad03219fa2901fe12b70
Jared Wasinger <j-wasinger@hotmail.com>
2014-03-13 19:17:44 -0700

    Some minor changes to the list tables.  There is a problem with the admin page being redirected through CAS instead of going to the default django admin page.

--- list_app/models.py
+++ list_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+class ListEntry(models.Model):
+    name = models.CharField(max_length=64)
+    # create_date = models.DateTimeField('date created')
+    # active_date = models.DateTimeField('date of last list activity')
+    # expire_date = models.DateTimeField('date of expiration')
+    date = models.DateTimeField('date created')
+
+    class Meta:
+        ordering = ('name',)
+
+class OwnerEntry(models.Model):
+    name = models.CharField(max_length=32)
+    list = models.ForeignKey(ListEntry)
+
+    class Meta:
+        ordering = ('name',)
+
+# class ListEntry(models.Model):
+#     name = models.CharField(max_length=64)
+#     owners = models.CharField(max_length=256)
+#     create_date = models.DateTimeField('date created')
+#     active_date = models.DateTimeField('date of last list activity')
+#     expire_date = models.DateTimeField('date of expiration')
+#     creator = models.CharField(max_length=64)
+#     descr = models.CharField(max_length=256)
+#     notes = models.CharField(max_length=2048)
+#     automated = models.IntegerField(max_length=11)
+#     server = models.CharField(max_length=64)
+#     data1 = models.CharField(max_length=256)
+#     data2 = models.CharField(max_length=256)
+#
+#     testKeys = models.ManyToManyField('TestEntry')
+#
+#     def HasOwner(self, name):
+#         owner_list = self.owners.split(', ', len(owners))
+#         for owner in owner_list:
+#             if owner == name:
+#                 return True
+#
+#         return False
+
 # Create your models here.
--- list_app/views.py
+++ list_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import ListEntry, OwnerEntry
 
 # Create your views here.
 
 def login_success(request):
-    print(request.user)
+    # u = OwnerEntry(name=request.user)
+    # print(u.name)
+    # print(u.lists)
+    # if(u == None):
+    #     print("no lists for user")
+    # else:
+    #     print(u.listentry_set.all())
+
+    records = OwnerEntry.objects.filter(name=request.user)
+    if(records == None):
+        print("no lists for:")
+        print(request.user)
+    else:
+        for oe in records:
+            print(oe.list.name)
+
     return HttpResponse("login success!")
