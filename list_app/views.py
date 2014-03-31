0a2bdc27c6cdf6946f88432edc3a86316cc4b0c6
Your name <j-wasinger@hotmail.com>
2014-03-30 18:29:13 -0700

    .

--- list_app/views.py
+++ list_app/views.py
@@ -3,19 +3,18 @@ from django.http import HttpResponse, HttpResponseRedirect
 from models import ListEntry, OwnerEntry
 from django.template import RequestContext, loader
 
-# Create your views here.
 
-
+#view for the page that is redirected to after successful CAS authentication
 def login_success(request):
-    oe_records = OwnerEntry.objects.filter(name=request.user)
+    user_lists = OwnerEntry.objects.filter(name=request.user)
     lists = []
-    for oe in oe_records:
-        lists.append(oe.list)
+    for list_record in user_lists:
+        lists.append(list_record.list)
 
-    if not oe_records:
+    if not user_lists:
         return HttpResponse(str.format("no lists for {0}", request.user))
-    else:
 
+    else:
         template = loader.get_template('login_success.html')
         context = RequestContext(request, {
             'lists': lists,
@@ -24,30 +23,38 @@ def login_success(request):
         return HttpResponse(template.render(context))
 
 
+def validate_list_changes():
+    return "success"
+
+
 def list_edit(request):
     if request.method == "POST":  # changes to a list have been submitted, TODO: check the validity of submitted data
-        return HttpResponseRedirect("app/login_success")
+        error_msg = validate_list_changes()
+
+        if error_msg == 'success':
+            return HttpResponseRedirect("app/login_success")
+        else:
+            #render list_edit.html with a mean-looking red warning
+            return HttpResponse("hello world!")
     else:
         admin_name = request.GET['admin_name']
         list_id = request.GET['list_id']
 
-        print(list_id)
-        print(admin_name)
-        #^^ these two lines could possibly cause a key error
-
-        admin_lists = ListEntry.objects.filter(name=admin_name)
         list_to_edit = ListEntry.objects.filter(id=list_id)
-        # list_to_edit = None
-        # for l in admin_lists:
-        #     if l.id == list_id:
-        #         list_to_edit = l
 
         if not list_to_edit:
             return HttpResponse(str.format("user {0} is not an administrator of {1}", admin_name, list_to_edit.name))
+            # ^^ I don't think this will ever be executed...
 
         template = loader.get_template('list_edit.html')
         context = RequestContext(request, {
-            'list_to_edit': list_to_edit
+            'admin_name': admin_name,
+            'list_to_edit': list_to_edit,
+            'invalid_edit': False,
         })
 
         return HttpResponse(template.render(context))
+
+def list_edit_cancel(request):
+
+    template = loader.get_template('list_edit.html')
