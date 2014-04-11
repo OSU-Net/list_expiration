1e8afc1cf611ccd4161ca732c097c1a2cc2b0526
Your name <j-wasinger@hotmail.com>
2014-04-11 11:44:06 -0700

    .

--- list_app/static/text_area.js
+++ list_app/static/text_area.js
@@ -1,14 +1,19 @@
-$('#expire_date').focusin(function(){
-	asdfasdf
-	if(this.value == '0')
-	{
-		this.value = '';
-		showCalendarControl(this);
-	}
-}).focusout(function(){
-	if( this.value == '')
-	{
-		this.value = '0';
-		hideCalendarControl(this);
-	}
+$("#list_edit_form.value").on('focus', function(){
+
+    console.log("hello world! focus");
+
+    if(this.value == 'default_value')
+    {
+        this.value = '';
+        showCalendarControl(this);
+    }
+}).on('blur', function(){
+
+    console.log("hello world! blur");
+
+    if( this.value == '')
+    {
+        this.value = 'default_value';
+        hideCalendarControl(this);
+    }
 })
