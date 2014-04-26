6a327828a97010917ccc9ec2c7fb0e212bea33d1
jared <j-wasinger@hotmail.com>
2014-04-25 21:57:10 -0700

    .

--- list_app/static/index.js
+++ list_app/static/index.js
@@ -5,10 +5,51 @@ var App =
         edit_element: null,
     };
 
+var text_area_stored_text = null;
+
 function get_button_list_id(button)
 {
     var id_str = button.id;
     return id_str.split("_")[2];
+}
+
+function on_text_area_gain_focus(text_area)
+{
+    console.log("on_focus");
+
+    text_area_stored_text = text_area.value;
+    text_area.value = '';
+    showCalendarControl(this);
+}
+
+function on_text_area_lose_focus(text_area)
+{
+    console.log("on_blur");
+    if( this.value == '')
+    {
+        this.value = App.default_text;
+        hideCalendarControl(this);
+    }
+}
+
+function get_date_delta(earlier_date, later_date)
+{   
+    return new Date(later_date.getTime() - earlier_date.getTime());
+}
+
+function validate_list_edit(list_id, expire_date_str)
+{
+    var new_expire_date = Date(expiration_date_str);
+    var current_expire_date = email_lists.get_list_by_id(list_id);
+    var delta_years = get_date_delta(current_expire_date,new_expire_date).getYears();
+
+    if(delta_years >= 2 || delta_years <= 0)
+    {
+        alert("Invalid expiration date!  Choose a date within two years of the current expiration date.");
+        return false;
+    }
+
+    return true;
 }
 
 function on_edit_button_click()
@@ -18,7 +59,7 @@ function on_edit_button_click()
 
     if(App.editing_list)
     {  
-        list_expire_html = $("div[id=".concat(id).concat("] div[name=expire_date]"));
+        list_expire_html = $("form[name=edit_form]"));
 
         var list_expire_date = email_lists.get_list_by_id(id).expire_date;
         var normal_html = 
@@ -28,7 +69,8 @@ function on_edit_button_click()
             </div>");
         list_expire_html.replaceWith(normal_html);
         $(this).html("Edit");
-        
+
+
         App.editing_list = false;
     }
     else
@@ -36,11 +78,15 @@ function on_edit_button_click()
         list_expire_html = $("div[id=".concat(id).concat("] div[name=expire_date]"));
         
         var editing_html =  
-            "<div name=\"expire_date\" class=\"list_field\">                                                    \
-                <b>List Expiration Date:</b><br>                                                                \
-                <textarea name=\"expire_date\" class=\"text_edit_field\"> Choose an expiration date</textarea>  \                                             \
-            </div>";
+        "<form name=\"edit_form\" action = \"lists\\submit_list_edit\">   
+            <div name=\"expire_date\" class=\"list_field\">                                                            \
+                <b>List Expiration Date:</b><br>                                                                       \
+                <input type=\"text\" name=\"expire_date\" class=\"text_edit_field\"> Choose an expiration date</input> \                                             \
+            </div>                                                                                                     \
+        </form>";
         list_expire_html.replaceWith(editing_html);
+
+        $("input[name=expire_date]").on('focus', on_text_area_gain_focus).on('blur', on_text_area_lose_focus);
 
         $(this).html("Save");
         
--- list_app/urls.py
+++ list_app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^index', 'list_app.views.login_success'),
-    url(r'^list_edit', 'list_app.views.list_edit'),
+    url(r'^list_edit', 'list_app.views.submit_list_edit'),
 )
