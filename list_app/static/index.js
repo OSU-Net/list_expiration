8fca02ab0a58872113d21dfcd0dac0bae05d3f4e
jared <j-wasinger@hotmail.com>
2014-04-30 16:22:52 -0700

    Added cancel button (not working).  Reformatted/commented some code to make it more readable.

--- list_app/static/index.js
+++ list_app/static/index.js
@@ -22,6 +22,7 @@ function on_text_area_gain_focus(text_area)
 
 function on_text_area_lose_focus(text_area)
 {
+    console.log("on_blur");
     if( this.value == '')
     {
         this.value = App.default_text;
@@ -29,32 +30,70 @@ function on_text_area_lose_focus(text_area)
     }
 }
 
+//Performs two functions: modifies list index page visually to open editing options.  Also signals to 'email_lists'
+//to begin editing on list with id 'id'
+function begin_editing(id)
+{
+    list_expire_html = $("div[id=".concat(id).concat("] div[name=expire_date]"));
+    
+    //add in a text field to allow modification of the expiration date
+    var editing_html =  
+    "<form name=\"edit_form\" action = \"lists\\submit_list_edit\" method=\"post\">                                   \
+        <div name=\"expire_date\" class=\"list_field\">                                                               \
+            <b>List Expiration Date:</b><br>                                                                          \
+            <input type=\"text\" name=\"expire_date\" class=\"text_edit_field\" value=\"choose an expiration date\">  \                                             \
+        </div>                                                                                                        \
+        <input type=\"hidden\" name=\"list_id\" value=\"".concat(id).concat("\" />                                    \                                                                                                               \
+    </form>");
+    list_expire_html.replaceWith(editing_html);
+
+    $("input[name=expire_date]").on('focus', on_text_area_gain_focus).on('blur', on_text_area_lose_focus);
+
+    //change the button text to 'save' and add a cancel button 
+    var button = $(":button[name=edit_button]");
+    button.html("Save");
+    button.after("<button id=\"cancel_button_".concat(id).concat("\" class=\"cancel_button\" name=\"cancel\"> cancel </button>"));
+    
+    email_lists.start_editing(id);
+}
+
+//stops editing list with id 'id'
+function end_editing(id)
+{
+    email_lists.end_editing();
+
+    list_expire_html = $("form[name=edit_form]");
+
+    //change the edit text area back into a readonly div
+    var list_expire_date = email_lists.get_list_by_id(id).expire_date;
+    var normal_html = 
+        "<div name=\"expire_date\" class=\"list_field\"> \
+            <p><b>List Expiration Date:</b></p>          \
+            <p>".concat(list_expire_date).concat("</p>   \
+        </div>");
+    list_expire_html.replaceWith(normal_html);
+
+    //Change 'Save' button back to 'Edit' and remove the cancel button 
+    var save_button = $(":button[name=save_button]");
+    save_button.html("Edit");
+    $("button[id=\"cancel_button_".concat(id).concat("\"]")).remove();
+}
+
+function on_cancel_button_click()
+{
+    var id = get_button_list_id(this);
+    end_editing(id);
+}
+
 function on_edit_button_click()
 {
     var id = get_button_list_id(this);
-    var list_expire_date = email_lists.get_list_by_id(id).get_expire_date_str();
-
     var list = email_lists.get_list_being_edited();
-
+    
     if(list == null)
     {  
-        list_expire_html = $("div[id=".concat(id).concat("] div[name=expire_date]"));
-        
-        var editing_html =  
-        "<form name=\"edit_form\" action = \"lists\\submit_list_edit\" method=\"post\">                                                          \
-            <div name=\"expire_date\" class=\"list_field\">                                                                                      \
-                <b>List Expiration Date:</b><br>                                                                                                 \
-                <input type=\"text\" name=\"expire_date\" class=\"text_edit_field\" value=\"".concat(list_expire_date).concat("\">               \                                             \
-            </div>                                                                                                                               \
-            <input type=\"hidden\" name=\"list_id\" value=\"").concat(id).concat("\" />                                                          \                                                                                                               \
-        </form>");
-        list_expire_html.replaceWith(editing_html);
-
-        $("input[name=expire_date]").on('focus', on_text_area_gain_focus).on('blur', on_text_area_lose_focus);
-
-        $(this).html("Save");
-        
-        email_lists.start_editing(id);
+        begin_editing(id);
+        var i = 0;
     }
     else
     {
@@ -64,18 +103,7 @@ function on_edit_button_click()
             return; //can't edit two lists at once!
         }
 
-        email_lists.end_editing();
-
-        list_expire_html = $("form[name=edit_form]");
-
-        var list_expire_date = email_lists.get_list_by_id(id).expire_date;
-        var normal_html = 
-            "<div name=\"expire_date\" class=\"list_field\"> \
-                <p><b>List Expiration Date:</b></p>          \
-                <p>".concat(list_expire_date).concat("</p>   \
-            </div>");
-        list_expire_html.replaceWith(normal_html);
-        $(this).html("Edit");
+        end_editing(id);
     }
 }
 
@@ -83,6 +111,7 @@ $(document).ready(function()
 {
     init();
     $(':button[name=edit_button]').click(on_edit_button_click);
+    //$(':button[name=cancel_button]').click(on_cancel_button_click);
 });
 
 function init()
