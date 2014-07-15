95d8f2d3b20963dfad2cdfc7ccfc4ea2695768c6
jared <j-wasinger@hotmail.com>
2014-07-15 12:17:51 -0700

    .

--- list_app/list_status.py
+++ list_app/list_status.py
@@ -1,28 +1,43 @@
 from list_app import models
+import pdb
 
 class ListStatus:
-	
-	name = None
-	owners = []
+    
+    name = None
+    owners = []
 
-	def __init__(self, name, owners):
-		self.name = name
-		self.owners = owners
+    def __init__(self, name, owners):
+        self.name = name
+        self.owners = owners
 
 class OwnerStatus:
-	
-	email = None
-	status = None
+    
+    email = None
+    status = None
+    is_onid = False
 
-	def __init__(self, email, status):
-		self.email = email
-		self.status = status
+    def __init__(self, email, status):
+        self.email = email
+        self.status = status
+        self.is_onid = is_owner_onid(email)
+
+#three onid endings
+#onid.orst.edu
+#oregonstate.edu
+#onid.oregonstate.edu
+#TODO: handle tooltips and account for more types of onid addresses
+def is_owner_onid(email):
+    strs = email.split('@')
+    if strs[1] == 'onid.orst.edu' or strs[1] == 'oregonstate.edu' or strs[1] == 'onid.oregonstate.edu':
+        return True
+
+    return False
 
 def calc_owner_status(owner):
-	if owner.bounced:
-		return "bounced"
+    if owner.bounced:
+        return "bounced"
 
-	if owner.onid_email == "":
-		return "no_response"
+    if owner.onid_email == "":
+        return "no_response"
 
-	return "claimed"
+    return "claimed"
