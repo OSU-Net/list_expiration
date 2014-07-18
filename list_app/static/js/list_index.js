0f6d96179bf3e977ecd1c2c2c8acfaba370e57d0
jared <j-wasinger@hotmail.com>
2014-07-18 14:13:41 -0700

    .

--- list_app/static/js/list_index.js
+++ list_app/static/js/list_index.js
@@ -8,24 +8,24 @@ function get_element_list_id(element)
     return id_strs[id_strs.length - 1];
 }
 
-function on_text_area_gain_focus(text_area)
-{
-    console.log("on_focus");
+// function on_text_area_gain_focus(text_area)
+// {
+//     console.log("on_focus");
 
-    text_area_stored_text = text_area.value;
-    text_area.value = '';
-    showCalendarControl(this); //should 'this' be 'text_area'?
-}
+//     text_area_stored_text = text_area.value;
+//     text_area.value = '';
+//     showCalendarControl(this); //should 'this' be 'text_area'?
+// }
 
-function on_text_area_lose_focus(text_area)
-{
-    console.log("on_blur");
-    if( this.value == '')
-    {
-        this.value = App.default_text;
-        hideCalendarControl(this); //should 'this' be 'text_area'?
-    }
-}
+// function on_text_area_lose_focus(text_area)
+// {
+//     console.log("on_blur");
+//     if( this.value == '')
+//     {
+//         this.value = App.default_text;
+//         hideCalendarControl(this); //should 'this' be 'text_area'?
+//     }
+// }
 
 function on_cancel_button_click()
 {
