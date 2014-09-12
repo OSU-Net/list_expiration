4dc9d33e25f071918d63dffd65b8d61d3f82b5eb
Jared Wasinger <j-wasinger@hotmail.com>
2014-09-12 12:26:52 -0700

    formatted top_hat.html to make it more readable. added 'test_index' view to start creating a new look for the list_index page while keeping the old one intact for easy reference.

--- list_app/urls.py
+++ list_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     url(r'^index/$', 'list_app.views.list_index', name='list_index'),
     url(r'^list_edit/$', 'list_app.views.submit_list_edit'), #should these last two urls have a trailing '/'?
     url(r'^onid_transition/$', 'list_app.views.onid_transition', name='onid_transition'),
-    url(r'^no_onid/$', 'list_app.views.no_onid', name='no_onid')
+    url(r'^no_onid/$', 'list_app.views.no_onid', name='no_onid'),
+    url(r'^test_index/$', 'list_app.views.test_index', name='test_index')
 )
--- list_app/views.py
+++ list_app/views.py
@@ -17,6 +17,11 @@ import datetime
 
 import pdb
 
+def test_index(request):
+    template = loader.get_template('index.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
 def no_onid(request):
     user_code = request.GET.get('id', '')
 
